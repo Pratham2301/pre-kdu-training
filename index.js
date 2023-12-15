85208f3a7692061211a83619a4fa7f2cfdb3e368
Pratham2301 <prathamrajbhoj2003@gmail.com>
2023-12-15 22:29:20 +0530

    PRE-KDU-JS-1 done..

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const inc_btn = document.getElementById('incr');
 const dec_btn = document.getElementById('decr');
 const number = document.getElementById('num');
 
-inc_btn.onclick = ()=>{
+inc_btn.onclick = () => {
     console.log("clicked+")
     number.innerHTML = parseInt(number.innerHTML) + 1;
 }
 
-dec_btn.onclick = ()=>{
+dec_btn.onclick = () => {
     console.log("clicked-")
     number.innerHTML = parseInt(number.innerHTML) - 1;
 }
